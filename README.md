cb247d3511b0b84b4b24730e86f717353d8c4d68
Tien Le <71688550+tientle@users.noreply.github.com>
2026-09-11 21:19:15 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,115 @@
 # IDS-706-Assignment-2-Data-Analysis
+
+## Overview
+
+This project explores data analysis, data engineering, and machine learning concepts using the Yelp Open Dataset.
+
+https://business.yelp.com/data/resources/open-dataset/
+
+This assignment includes 1. an analysis between restaurant attributes and Yelp star ratings, 2. a performance between Pandas and Polars, and 3. experimentation with Rust's ownership system.
+
+## Analysis between restaurant attributes and Yelp star ratings
+
+### Project Question/Goal
+
+What restaurant characteristiscs are associated with higher Yelp star ratings?
+
+I analyzed restaurant attributes such as outdoor seating, delivery, alcohol offerings, bik parking and other characteristics to explore their relationships with a restaurant's Yelp star rating.
+
+I trained a Random Forest regression model to deterimine specifically which attributes were most useful in predicting restaurant ratings.
+
+### Data
+
+The analysis uses business data from Yelp Open Dataset. The data contains many types of business, including beauty salons and dentists, so I filtered the data to:
+
+- Businesses categorized as 'Restaurant'
+- Restaurants with at least 500 reviews
+
+This led me to have 1,263 restaurants to work with.
+
+### Data Cleaning
+
+The 'attributes' column contains restaurant characteristics stored in a dictionary-like structure.
+
+To clean and prepare the data, I:
+
+1. Expanded attributes into individual columns
+2. Cleaned inconsistent string representations
+3. Calculated the percentage of each attribute to see which ones had the highest amount of data
+4. Removed attributes that did not have a high percentage
+5. Focused on simple attributes rather than nested dictionary attributes
+6. Converted 'RestaurantsPriceRange2' to a numeric variable
+
+Missing values were kept where appropriate because the absence of a characteristic in Yelp's data doesn't necessarily mean that the restaurant doesn't have that characteristic.
+
+### Machine Learning
+
+I used a Random Forest regression model to predict Yelp star ratings using restaurant attributes.
+
+Target variable: 'stars'
+Predictors: cleaned list of restaurant attributes
+
+Categorical attributes were converted into numeric features using one-hot encoding. 'RestaurantsPriceRange2' was converted to numeric values and passed through without one-hot encoding.
+
+The dataset was split into 80% training data and 20% testing data.
+
+The Random Forest model used 200 decision trees.
+
+The model received a Mean Absolute Error (MAE) of 0.322 stars, meaning that the model's predicted rating differed from the actual Yelp rating by 0.32 stars on average.
+
+To identify the restaurant characteristics most useful for predicting Yelp ratings, I used Random Forest feature importance.
+
+One-hot encoding creates multiple model features from a single restaurant attribute, so I grouped the importance scores of the encoded featuures back into their original attribute.
+
+The highest-ranked attributes:
+
+1. Alcohol
+2. Outdoor Seating
+3. Happy Hour
+4. Restaurant Delivery
+5. TV Availability
+6. Bike Parking
+7. Dogs Allowed
+8. Smoking
+9. Wheelchair Accessibility
+10. Restaurant Table Service
+
+### Interpretation
+
+For attributes that Random Forest considered important, the differences in average ratings were generally small.
+
+Feature importance represents how useful a variable is for prediction, but does not indicate whether an attribute would cause a rating to increase or decrease. Differences in average ratings represent associations rather than causal relationships.
+
+![Average Yelp Rating by Alcohol Offering](images/alcohol_ratings.png)
+
+![Average Yelp Rating by Outdoor Seating Offering](images/outdoor_ratings.png)
+
+## Performance between Pandas and Polars
+
+I compared performance between Pandas and Polars by 1. filtering data and 2. grouping data and calculating summary statistics. The differences in each were small, but performance depends on many factors such as dataset size and system environments.
+
+Pandas filtering time: 0.020480 seconds
+Polars filtering time: 0.019030 seconds
+
+Pandas grouping and summary statistics time: 0.016093 seconds
+Polars grouping and summary statistics time: 0.016181 seconds
+
+## Experimentation with Rust's ownership system
+
+I modified a Rust Jupyter notebook to demonstrate Rust's ownership system. I created a vector of colors. I assigned the vector to another variable to transfer ownership. After this, the original vector can no longer be used.
+
+## Reproducing Analysis
+
+1. Clone repository
+
+2. Create a virtual environment
+   python3 -m venv .venv
+
+3. Activate the environment
+   source .venv/bin/activate
+
+4. Install dependencies
+   pip install -r requirements.txt
+
+5. Run analysis
+   python yelp_data.py
---
+++ yelp_data.py
@@ -0,0 +1,351 @@
+"""
+File: yelp_data.py
+Author: Tien Le
+Date: 2026-09-09
+
+"""
+
+# --- Import libraries ---
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+import polars as pl
+import time
+
+
+# ==============================================================================
+# 1. Import the dataset
+# ==============================================================================
+data = pd.read_json('./data/yelp_academic_dataset_business.json', lines=True)
+
+
+# ==============================================================================
+# 2. Inspect the data
+# ==============================================================================
+
+print(data.shape) # Print shape: We get 150,346 rows and 14 columns.
+print(data.columns.tolist()) # View column names as list
+
+# Explore the the categories column. From this, we can see that this dataset includes business categories that span from nail salons to restaurants. 
+print(data["categories"].unique()) 
+print(data["categories"].value_counts()) 
+
+
+# ==============================================================================
+# 3. Basic Filtering and Grouping
+# ==============================================================================
+
+# For this assignment, I will focus on a subset of data that is just the restaurants.
+
+restaurant_mask = data["categories"].str.contains("Restaurants", na=False) # Make a filter for categories containing 'Restaurant'
+
+review_count_mask = data["review_count"] >= 500 # Make a filter businesses with 500 or more reviews
+
+data = data[restaurant_mask & review_count_mask] # Data that is only restaurants with 500 or more reviews
+
+print(data.head()) # Inspect the subset of data. 
+
+print(data.shape) # The shape has now been reduced to (1263, 14).
+
+# Exploring which state is most prevalent with restaurants with more than 500 reviews
+print(data["state"].value_counts()) 
+print(data.info())
+print(data.describe())
+
+data["attributes"].iloc[0] # Explore what is in the attributes field to explore what could be used in a ML algorithm
+
+attributes = data["attributes"].apply(pd.Series) # Unpack attributes into a more readable format and explore
+attributes.head()
+
+attributes.columns.tolist() # Turn attributes into list
+
+# After inspecting attributes, I noticed there are some cleaning/formatting issues with values like u'none', and NaN 
+clean_attributes = attributes.copy() # Create a copy of attributes to clean
+clean_attributes = clean_attributes.replace(["None", "None'", "u'None"], np.nan) # Replace None with NaN
+
+def clean_strings(value):
+    if isinstance(value, str):
+        value = value.strip()
+
+        if value.startswith("u'") and value.endswith("'"): # For strings like u'average', only keep average
+            value = value[2:-1]
+        elif value.startswith("'") and value.endswith("'"): # Remove ' ' quotes around string
+            value = value[1:-1]
+    return value
+
+clean_attributes = clean_attributes.map(clean_strings)
+
+# Fix True/False values from string to boolean
+clean_attributes = clean_attributes.replace({
+    "True": True,
+    "False": False
+})
+
+# Do a quick inspection of columns
+print(clean_attributes["WiFi"].value_counts(dropna=False))
+print(clean_attributes["NoiseLevel"].value_counts(dropna=False))
+print(clean_attributes["Alcohol"].value_counts(dropna=False))
+
+
+# To determine which attributes to focus on for the ML algorithm, we can calculate which attribute fields have the most values
+attribute_percent = (clean_attributes.notna().sum() / len(clean_attributes) * 100).sort_values(ascending=False)
+print(attribute_percent)
+
+# Now within the attributes with higher percentage, we can look at the variability in each attribute. It's not useful to know that 99% of businesses accept credit cards, so we can look for attributes that are more split.
+high_coverage = attribute_percent[attribute_percent >= 75].index
+
+for column in high_coverage:
+    print(f"\n{column}")
+    print(clean_attributes[column].value_counts(dropna=False))
+
+# For the purposes of this assignment, we can drop the fields with nested values
+nested_attributes = [
+    "BusinessParking",
+    "Ambience",
+    "GoodForMeal",
+    "Music",
+    "BestNights",
+    "DietaryRestrictions"
+]
+
+# Remove the nested attributes
+simple_attributes = clean_attributes.drop(columns=nested_attributes)
+
+# Join the original data with the simplified attributes data for analysis
+restaurants_analysis = data[["stars", "review_count"]].join(simple_attributes)
+
+simple_attributes_list = simple_attributes.columns.to_list()
+
+for attribute in simple_attributes_list:
+    print(f"\n . ݁₊ ⊹ . ݁ ⟡ ݁ {attribute} . ݁₊ ⊹ . ݁ ⟡  ")
+    summary = restaurants_analysis.groupby(attribute,dropna=False)["stars"].agg(["count", "mean", "median"])
+    print(summary)
+
+# ==============================================================================
+# 4. Explore a Machine Learning Algorithm
+# ==============================================================================
+
+# Use a machine learning algorithm to see if the algorithm can predict a star ratings based on a combination of attributes
+x = restaurants_analysis[simple_attributes_list]
+y = restaurants_analysis["stars"]
+
+categorical_columns = x.select_dtypes(include=["object", "str", "bool"]).columns 
+# Filter for categorical columns. RestaurantsPriceRange2 has oridinal values.
+categorical_columns = [col for col in categorical_columns if col != "RestaurantsPriceRange2"]
+# Turns "1" "2" into actual 1, 2 instead of strings 
+x["RestaurantsPriceRange2"] = pd.to_numeric(x["RestaurantsPriceRange2"], errors="coerce")
+
+# Use One-Hot Encoding to encode restaurant attributes into numeric columns. 
+preprocessor = ColumnTransformer(
+    transformers=[
+        ( "categorical", OneHotEncoder(handle_unknown="ignore"), categorical_columns)
+    ],
+    remainder="passthrough" # Non-categorical columns (price range) are passed through unchanged
+)
+
+# Apply preprocessing steps to predictor variable: restaurant attributes
+x_encoded = preprocessor.fit_transform(x)
+x_encoded.shape # Check shape of encoded dataset
+
+
+# Split the data into training and testing sets, 80% and 20% respectively. 
+# Random state set for reproducibility 
+x_train, x_test, y_train, y_test = train_test_split(
+    x_encoded,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Create a Random Forest Regression model using 200 decision trees
+random_forest_model = RandomForestRegressor(n_estimators=200, random_state=42)
+
+# Train the model using training data
+random_forest_model.fit(x_train, y_train)
+
+# Use the trained model to predict ratings for the test data
+rating_prediction = random_forest_model.predict(x_test)
+
+print("Actual:", y_test.iloc[:5].values)
+print("Predicted:", rating_prediction[:5])
+
+mae = mean_absolute_error(y_test, rating_prediction)
+print("Mean Absolute Error:", mae)
+
+
+# Get the names and feature importance scores of the model features
+feature_names = preprocessor.get_feature_names_out()
+
+feature_importance = pd.DataFrame({
+    "feature": feature_names,
+    "importance": random_forest_model.feature_importances_})
+
+feature_importance = feature_importance.sort_values(
+    "importance",
+    ascending=False)
+
+print(feature_importance.head(10))
+
+# Match each encoded feature back to the original restaurant attribute. Ex. categorical__OutdoorSeating_False and categorical__OutdoorSeating_True are separate right now, so combine them back to be Outdoor Seating overall)
+def get_original_attribute(feature):
+    for attribute in simple_attributes_list:
+        if attribute in feature:
+            return attribute
+
+feature_importance["attribute"] = feature_importance["feature"].apply(get_original_attribute)
+
+# Combine the scores of the encoded categories that fall under the same attribute
+attribute_importance = (
+    feature_importance
+    .groupby("attribute")["importance"]
+    .sum()
+    .sort_values(ascending=False)
+)
+
+# Get the top 10 restaurant attributes
+top_attributes = attribute_importance.head(10).index.tolist()
+
+# Calculate average rating and restauarant count for each value within the top 10 attributes
+results = []
+
+for attribute in top_attributes:
+    summary = (
+        restaurants_analysis
+        .groupby(attribute)["stars"]
+        .agg(["mean", "count"])
+        .reset_index()
+    )
+    
+    summary["attribute"] = attribute
+    summary = summary.rename(columns={attribute: "value"})
+        
+    results.append(summary)
+
+# Create new dataframe
+attribute_ratings = pd.concat(results, ignore_index=True)
+
+print(attribute_importance.head(10))
+
+# ==============================================================================
+# Visualizations
+# ==============================================================================
+
+# Alcohol visualization
+alcohol_data = attribute_ratings[attribute_ratings["attribute"] == "Alcohol"].sort_values("mean", ascending=False)
+alcohol_data = alcohol_data.copy()
+
+alcohol_data["value"] = alcohol_data["value"].replace({
+    "full_bar": "Full Bar",
+    "beer_and_wine": "Beer & Wine",
+    "none": "No Alcohol"
+})
+plt.figure(figsize=(10, 4))
+
+bar = plt.bar(
+    alcohol_data["value"].astype(str),
+    alcohol_data["mean"],
+    width=0.4,
+    color='pink'
+    )
+plt.bar_label(
+    bar,
+    fmt="%.2f",
+    padding=3
+)
+plt.title("Average Yelp Rating for Restaurants with 500+ Reviews, by Alcohol Offering")
+plt.xlabel("Alcohol Offering")
+plt.ylabel("Average Yelp Rating", rotation=0,
+           labelpad=60)
+plt.ylim(3, 5)
+plt.xticks(rotation=0)
+plt.tight_layout()
+
+plt.savefig(
+    "images/alcohol_ratings.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+
+# Outdoor Seating Visualization
+outdoor_seating_data = attribute_ratings[attribute_ratings["attribute"] == "OutdoorSeating"].sort_values("mean", ascending=False)
+outdoor_seating_data = outdoor_seating_data.copy()
+
+plt.figure(figsize=(10, 4))
+
+bar = plt.bar(
+    outdoor_seating_data["value"].astype(str),
+    outdoor_seating_data["mean"],
+    width=0.4,
+    color='pink'
+    )
+plt.bar_label(
+    bar,
+    fmt="%.2f",
+    padding=3
+)
+plt.title("Average Yelp Rating for Restaurants with 500+ Reviews, by Outdoor Seating Offering")
+plt.xlabel("Outdoor Seating Offering")
+plt.ylabel("Average Yelp Rating", rotation=0,
+           labelpad=60)
+plt.ylim(3, 5)
+plt.xticks(rotation=0)
+
+plt.tight_layout()
+
+plt.savefig(
+    "images/outdoor_ratings.png",
+    dpi=300,
+    bbox_inches="tight"
+)
+
+
+# plt.show()
+
+
+# ==============================================================================
+# Comparing pandas and polars
+# ==============================================================================
+
+# --- Test performance of filtering data ---
+data_pandas = pd.read_json("./data/yelp_academic_dataset_business.json",lines=True)
+
+start = time.perf_counter()
+restaurants_pandas = data_pandas[data_pandas["categories"].str.contains("Restaurants", na=False)
+                          &
+                          data_pandas["review_count"] >= 500]
+
+pandas_time_filter = time.perf_counter()-start
+print(f"Pandas: {pandas_time_filter:.6f} seconds")
+
+
+data_polars = pl.read_ndjson('./data/yelp_academic_dataset_business.json')
+start = time.perf_counter()
+restaurants_polars = data_polars.filter(
+    pl.col("categories").str.contains("Restaurants")
+    & 
+    (pl.col("review_count") >= 500)
+)
+polars_time_filter = time.perf_counter()-start
+print(f"Polars: {polars_time_filter:.6f} seconds")
+
+# --- Test performance of grouping data and calculating summary statistics ---
+start = time.perf_counter()
+pandas_group_summary = (data_pandas.groupby("state")["stars"].agg(["mean", "count"]))
+pandas_group_summary_time = time.perf_counter()-start
+
+print(f"Pandas grouping and summary statistics: {pandas_group_summary_time:.6f} seconds")
+
+start = time.perf_counter()
+polars_group_summary = (data_polars.group_by("state").agg(pl.col("stars").mean().alias("mean"), pl.col("stars").count().alias("count")))
+polars_group_time = time.perf_counter()-start
+
+print(f"Polars grouping and summary statistics: {polars_group_time:.6f} seconds")
+
+
